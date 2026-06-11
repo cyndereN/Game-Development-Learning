fec80dba1b65f71aa99f006c38379e66ae7baf43
cyndereN <2862087301@qq.com>
2026-06-11 01:23:20 +0100

    Remove UE demo projects and assets / Initial GA learning
    Delete bundled Unreal Engine demo projects (Car_Demo and Game_Mode_Demo) along with their Config and Content directories. This removes numerous .uproject, .ini, .uasset and .umap files (mannequin, starter content, vehicle/third-person assets, maps, materials, etc.) to clean up repository and reduce repo size.

--- Projects/UE/Game_Mode_Demo/Source/Game_Mode_Demo/Game_Mode_Demo.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Game_Mode_Demo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game_Mode_Demo, "Game_Mode_Demo" );
